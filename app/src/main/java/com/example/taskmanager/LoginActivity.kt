191662ab98388ae78d6f9dd0fa41a48f5417df71
japneet <japneetsaini084@gmail.com>
2023-05-01 21:25:13 +0530

    Created fragments and navigation menu

---
+++ app/src/main/java/com/example/taskmanager/LoginActivity.kt
@@ -0,0 +1,14 @@
+package com.example.taskmanager
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.android.gms.auth.api.identity.BeginSignInRequest
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+
+    }
+}
--- app/src/main/java/com/example/taskmanager/MainActivity.kt
+++ app/src/main/java/com/example/taskmanager/MainActivity.kt
@@ -2,10 +2,34 @@ package com.example.taskmanager
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var bottomNavMenu: BottomNavigationView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        bottomNavMenu = findViewById(R.id.bottomNavMenu)
+        loadFrag(HomeFragment())
+        bottomNavMenu.setOnItemSelectedListener {
+        when(it.itemId){
+            R.id.home -> {loadFrag(HomeFragment())}
+            R.id.addTask -> {loadFrag(AddTaskFragment())}
+            R.id.Task -> {loadFrag(TaskFragment())}
+            R.id.LogOut -> {}
+        }
+            true
+        }
+
+    }
+
+    fun loadFrag(frag: Fragment){
+        var myFrag = supportFragmentManager.beginTransaction()
+        myFrag.replace(com.google.android.material.R.id.container,frag)
+        myFrag.commit()
     }
 }
